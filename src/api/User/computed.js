4f5854c4d61d03a65979da86625df86dd77a2bc5
yhsol <yhsol1592@gmail.con>
2019-05-06 17:51:24 +0900

    add check following

---
+++ src/api/User/computed.js
@@ -0,0 +1,26 @@
+import { prisma } from "../../../generated/prisma-client";
+
+export default {
+  User: {
+    fullName: parent => {
+      return `${parent.firstName} ${parent.lastName}`;
+    },
+    isFollowing: async (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      try {
+        return await prisma.$exists.user({
+          AND: [{ id: user.id }, { id: parentId }]
+        });
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+    itsMe: (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      return user.id === parentId;
+    }
+  }
+};
