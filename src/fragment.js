846eb57f90d8902919c07ebc59e9b861be42113b
yhsol <yhsol1592@gmail.com>
2019-05-07 17:45:06 +0900

    add seeFullPost

--- src/fragment.js
+++ src/fragment.js
@@ -1,9 +1,35 @@
+export const USER_FRAGMENT = `
+        id
+        userName
+`;
+
 export const COMMENT_FRAGMENT = `
-    fragment CommentParts on Comment {
         id
         text
         user {
-            userName
+            ${USER_FRAGMENT}
         }
+`;
+
+export const FILE_FRAGMENT = `
+        id
+        url
+`;
+
+export const FULL_POST_FRAGMENT = `
+    fragment PostParts on Post {
+        id
+        location
+        caption
+        files {
+            ${FILE_FRAGMENT}
+        }
+        comments {
+            ${COMMENT_FRAGMENT}
+        }
+        user {
+            ${USER_FRAGMENT}
+        }
+        
     }
 `;
