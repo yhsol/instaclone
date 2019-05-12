3dd4cf087fab9ecf4473d6eb8ffef0dd3adf06be
yhsol <yhsol1592@gmail.com>
2019-05-12 15:41:03 +0900

    add requestSecret

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,6 +4,10 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { userName, email, firstName = "", lastName = "", bio = "" } = args;
+      const exists = await prisma.$exists.user({ userName, email });
+      if (exists) {
+        throw Error("This username is already taken");
+      }
       await prisma.createUser({
         userName,
         email,
