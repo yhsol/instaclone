8613ad2bd3cb5dc8ceb73bf1a64253503dcdf27d
yhsol <yhsol1592@gmail.com>
2019-05-12 13:28:39 +0900

    refactor create account

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,14 +4,14 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { userName, email, firstName = "", lastName = "", bio = "" } = args;
-      const user = await prisma.createUser({
+      await prisma.createUser({
         userName,
         email,
         firstName,
         lastName,
         bio
       });
-      return user;
+      return true;
     }
   }
 };
