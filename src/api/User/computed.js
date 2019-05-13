e67f32900461b2f37156fc513334de0b953f8a40
yhsol <yhsol1592@gmail.com>
2019-05-14 00:03:56 +0900

    add refactor UserProfile

--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -2,6 +2,27 @@ import { prisma } from "../../../generated/prisma-client";
 
 export default {
   User: {
+    post: ({ id }) => prisma.user({ id }).post(),
+    following: ({ id }) => prisma.user({ id }).following(),
+    follower: ({ id }) => prisma.user({ id }).follower(),
+    like: ({ id }) => prisma.user({ id }).like(),
+    comment: ({ id }) => prisma.user({ id }).comment(),
+    room: ({ id }) => prisma.user({ id }).room(),
+    postsCount: ({ id }) =>
+      prisma
+        .postsConnection({ where: { user: { id } } })
+        .aggregate()
+        .count(),
+    followingCount: ({ id }) =>
+      prisma
+        .usersConnection({ where: { followers_some: { id } } })
+        .aggregate()
+        .count(),
+    followersCount: ({ id }) =>
+      prisma
+        .usersConnection({ where: { following_none: { id } } })
+        .aggregate()
+        .count(),
     fullName: parent => {
       return `${parent.firstName} ${parent.lastName}`;
     },
